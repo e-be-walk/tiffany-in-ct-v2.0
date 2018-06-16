477fe6ba0ae9395febbb2ca698ac431dd0d81abd
e-be-walk <e.be.walk@gmail.com>
2018-06-15 21:09:10 -0400

    Working on users and related views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :password,
+      :site_admin,
+      :email
+    )
+  end
 end
