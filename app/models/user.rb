a23c44bb77ee8988825bc828729864b72c87a686
e-be-walk <e.be.walk@gmail.com>
2018-06-21 09:27:54 -0400

    Working on error messages/validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sites
   has_many :windows, through: :sites
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
