ca6177160754ec86a4000e8092635ca4e75d33c8
e-be-walk <e.be.walk@gmail.com>
2018-07-02 21:38:35 -0400

    Reviewing before review tomorrow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
         u.password = params[:code][0..71]
       end
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       @user = User.new
       redirect_to new_user_url
